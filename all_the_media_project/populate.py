dbda94b128958569211f96d576478e564e1b1427
Ross Barnie <r.barnie@gmail.com>
2013-03-11 12:06:51 +0000

    made populate script and index.html include Playlist.url

--- all_the_media_project/populate.py
+++ all_the_media_project/populate.py
@@ -15,12 +15,12 @@ up2 = UserProfile(user=u2)
 up2.save()
 print "created user " + u2.username + ", password " + u2.password
 
-p = Playlist(title='epic meal time', creator=u1, views=0)
+p = Playlist(title='epic meal time', url='epic_meal_time',creator=u1, views=0)
 p.save()
 print "created " + p.title + " playlist for " + p.creator.username
 Media(playlist=p, name='EMT - Date With Bacon', url='http://www.youtube.com/watch?v=86eVxuBkqeU').save()
 Media(playlist=p, name='EMT - Burly Beaver Sandwich', url='http://www.youtube.com/watch?v=7Ake5iEDtPM').save()
-p = Playlist(title='scott pilgrim songs', creator=u1, views=0)
+p = Playlist(title='scott pilgrim songs', creator=u1, url='scott_pilgrim_songs', views=0)
 p.save()
 print "created " + p.title + " playlist for " + p.creator.username
 Media(playlist=p, name='Garbage Truck', url='http://www.youtube.com/watch?v=fhGu2CDqQqo').save()
